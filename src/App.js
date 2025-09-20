afc58ea9bea41bbead4037d90a4b916f96c1dda6
ritapessoa01 <ritappessoa@gmail.com>
2025-09-20 01:34:16 +0100

    Added a Search Engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">My Dictionary</header>
+      <Dictionary />
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for the definition of ${keyword} `);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input
+          type="search"
+          onChange={handleKeywordChange}
+          placeholder="Enter a word..."
+        />
+        <input type="submit" />
+      </form>
+    </div>
+  );
+}

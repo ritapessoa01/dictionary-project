f8bd15ec9db34cecb7e9e0ca22140bda2c731c88
ritapessoa01 <ritappessoa@gmail.com>
2025-09-22 20:49:27 +0100

    Fixed footer issue

--- src/Footer.js
+++ src/Footer.js
@@ -4,18 +4,27 @@ export default function Footer() {
   return (
     <div>
       This project was coded by{" "}
-      <a href="https://github.com/ritapessoa01" target="_blank">
+      <a
+        href="https://github.com/ritapessoa01"
+        target="_blank"
+        rel="noreferrer"
+      >
         Rita Pessoa
       </a>
       , it is{" "}
       <a
         href="https://github.com/ritapessoa01/dictionary-project"
         target="_blank"
+        rel="noreferrer"
       >
         open sorced
       </a>{" "}
       and hosted on{" "}
-      <a href="https://ritas-dictionary.netlify.app/" target="_blank">
+      <a
+        href="https://ritas-dictionary.netlify.app/"
+        target="_blank"
+        rel="noreferrer"
+      >
         Netlify
       </a>
     </div>
